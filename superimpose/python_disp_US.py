8b13cb27e07ff1f70a8e66bc7caa1948a148e0b5
Anthony Wong <awong0811@gmail.com>
2025-01-15 20:04:39 -0800

    modified files

--- superimpose/python_disp_US.py
+++ superimpose/python_disp_US.py
@@ -5,28 +5,22 @@ from utils import *
 import cv2
 
 if __name__ == '__main__':
-    # Load the .mat file
     mat_contents = scipy.io.loadmat('./matlab_files/mid_saggital_image_data.mat')
-    #print ("Matrix Contents: ", mat_contents)
-    img_data = mat_contents['mid_sag_img_data']
-    print ("Image Data: ", img_data.shape)
+    raw_img = mat_contents['mid_sag_img_data']
+    print ("Image Data: ", raw_img.shape)
+    img_data = raw_img.copy()
 
-    newC=[206, 206, 206]
-    newscale=[0.0306, 0.0306, 0.0306]
+    # Apply smoothing
+    args_bf = {'type': 'bilateral_filter', 'd': 9, 'sigmaColor': 75, 'sigmaSpace': 75}
+    args_nlm = {'type': 'nl_means', 'h': 10, 'templateWindowSize': 7, 'searchWindowSize': 21}
+    smoothed = smooth(img_data, args_bf)
 
-    # Apply bilateral filter
-    d = 9              # Diameter of the pixel neighborhood
-    sigmaColor = 75    # Filter sigma in color space
-    sigmaSpace = 75    # Filter sigma in coordinate space
-    img_data = cv2.bilateralFilter(img_data, d, sigmaColor, sigmaSpace)
-    # Apply a threshold to mask the blue regions
-    threshold = np.percentile(img_data, 95)  # Adjust this percentile to isolate yellow regions
-    masked_img_data = np.where(img_data > threshold, img_data, np.nan)
-
-    print(masked_img_data.shape)
+    threshold = 170
+    thresholded = np.where(smoothed > threshold, smoothed, 0)
 
     # Display the image
-    #plt.imshow(img_data, extent=(1, newC[0]*newscale[0], 1, newC[1]*newscale[1]), aspect='auto')
-    plt.imshow(masked_img_data, extent=(1, newC[0]*newscale[0], 1, newC[1]*newscale[1]), aspect='auto')
+    newC=[206, 206, 206]
+    newscale=[0.0306, 0.0306, 0.0306]
+    plt.imshow(thresholded, extent=(1, newC[0]*newscale[0], 1, newC[1]*newscale[1]), aspect='auto')
     plt.colorbar()
     plt.show()
--- superimpose/utils.py
+++ superimpose/utils.py
@@ -1,3 +1,20 @@
 import cv2
 import numpy as np
 
+def smooth(img_data: np.ndarray, args: dict) -> np.ndarray:
+    type = args["type"]
+    args = {k: v for k, v in args.items() if k != 'type'}
+    # args.pop('type', None)
+    if type == 'bilateral_filter':
+        img_data = img_data.astype(np.float32)
+        # img_data = cv2.bilateralFilter(img_data, d=9, sigmaColor=75, sigmaSpace=75)
+        img_data = cv2.bilateralFilter(img_data, **args)
+        img_data = img_data.astype(np.float64)
+        return img_data
+    elif type == 'nl_means':
+        img_data = img_data.astype(np.uint8)
+        # img_data = cv2.fastNlMeansDenoising(img_data, None, h=10, templateWindowSize=7, searchWindowSize=21)
+        img_data = cv2.fastNlMeansDenoising(img_data, None, **args)
+        img_data = img_data.astype(np.float64)
+        return img_data
+
